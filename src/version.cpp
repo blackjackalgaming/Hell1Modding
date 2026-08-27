bed60faad95eec446724a886c80b2664a4dbac85
blackjackalgamingfb <blackjackalgamingfb@gmail.com>
2026-08-27 01:51:31 -0400

    deleted built version.cpp, build log

--- src/version.cpp
+++
@@ -1,9 +0,0 @@
-#include "version.hpp"
-
-namespace big
-{
-	const char* version::GIT_SHA1           = "eb4e296463a075ee818885786726ef0d82181a62-dirty";
-	const char* version::GIT_BRANCH         = "main";
-	const char* version::GIT_DATE           = "Tue Aug 25 23:04:40 2026";
-	const char* version::GIT_COMMIT_SUBJECT = "updated README";
-};
